7626d12533b6258452458998271a8111793d0753
Philip Clark <109166407+PhilipClark-SpamAccount@users.noreply.github.com>
2022-11-05 17:56:15 -0400

    Updates

---
+++ renderFx.js
@@ -0,0 +1,17 @@
+function renderFxToImg(imgId, duration, fxFolder, frameCount, frameName, delay) {
+  let currentFrame = 0;
+  setTimeout(() => {
+    setInterval(() => {
+      updateFrame(imgId, fxFolder, frameName, frameCount, currentFrame);
+      currentFrame++;
+    }, duration / frameCount);
+  }, delay);
+}
+
+function updateFrame(imgId, fxFolder, frameName, frameCount, _currentFrame) {
+  let nextFrame = _currentFrame + 1;
+  let img = document.getElementById(imgId);
+  if (nextFrame < frameCount) {
+    img.src = `/${fxFolder}/${frameName + nextFrame}.png`;
+  }
+}
--- todo.md
+++ todo.md
@@ -1,5 +1,7 @@
 # Todo
 
-- [ ] Add sound?
+- [x] Add sound?
+- [x] Add particles?
+- [x] Better responsiveness?
 - [ ] A mode where you pre-plan all three modes would be cool
 - [ ] online multiplayer?? Why not?
