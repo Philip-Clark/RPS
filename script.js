3c36c241d384d4251ae332d3fff2ff1e73465f55
Philip Clark <109166407+PhilipClark-SpamAccount@users.noreply.github.com>
2022-11-07 00:52:57 -0500

    code cleanup, win screen, and win streak

--- script.js
+++ script.js
@@ -4,87 +4,148 @@ const handMap = ['rock', 'paper', 'scissor'];
 
 let aiScore = 0;
 let playerScore = 0;
-
+let winStreak = 0;
+let choice = '';
 const audios = [
   new Audio('assets/sounds/punch1.mp3'),
   new Audio('assets/sounds/punch2.mp3'),
   new Audio('assets/sounds/punch3.mp3'),
 ];
 
-function startRound(choice) {
-  let buttons = [...document.getElementsByClassName('button')];
-  console.log(buttons);
+let buttons = [];
 
+// Timing
+const timeBetweenPounds = 200;
+const resetTime = 10;
+const attackDelay = 100;
+const attackSpeed = 500;
+const delayEndScreen = 400;
+const poundSpeed = 200;
+
+function restart() {
+  buttons = [...document.getElementsByClassName('button')];
+  playAnimation(
+    'endScreen',
+    'hideEndScreen cubic-bezier(0.42, -0.02, 0.82, 0.85) forwards',
+    100,
+    false
+  );
+  buttons.forEach((button) => (button.disabled = false));
+
+  aiScore = 0;
+  playerScore = 0;
+  updateScores();
+  resetHands();
+
+  setTimeout(() => {
+    document.getElementById('lose').style.visibility = 'hidden';
+    document.getElementById('win').style.visibility = 'hidden';
+  }, 150);
+}
+
+function startRound(_choice) {
+  choice = _choice;
   buttons.forEach((button) => {
     button.disabled = true;
-
-    setTimeout(() => {
-      button.disabled = false;
-      resetHands();
-    }, 1500);
   });
 
   poundCount = 3;
   resetHands();
 
-  playAnimation(choice);
+  animateRound();
 }
 
-function resetHands() {
-  hideHand('ai', 'fists');
-  hideHand('ai', 'paper');
-  hideHand('ai', 'scissor');
-  hideHand('player', 'fists');
-  hideHand('player', 'paper');
-  hideHand('player', 'scissor');
-
-  showHand('player', 'fist');
-  showHand('ai', 'fist');
-}
-
-function playAnimation(choice) {
-  let playerChoice = choice;
+function animateRound() {
   let fistArray = [...document.getElementsByClassName('fists')];
 
-  document.body.style.animation = 'screenShake 2000ms linear normal forwards';
-  let audio = audios[Math.floor(Math.random() * 3)];
+  document.body.style.animation = 'screenShake' + poundSpeed + 'ms linear normal forwards';
 
+  let audio = audios[Math.floor(Math.random() * 3)];
   fistArray.forEach((element) => {
-    element.style.animation = 'pound 2000ms linear normal forwards';
+    element.style.animation = 'pound ' + poundSpeed + 'ms linear normal forwards';
   });
-  renderFxToImg('playerDust', 300, 'assets/vfx/pound', 11, 'frame_0', 200);
-  renderFxToImg('AiDust', 300, 'assets/vfx/pound', 11, 'frame_0', 200);
-  audio.play();
-  audio.currentTime = 0;
+
+  renderFxToImg('playerDust', 200, 'assets/vfx/pound', 11, 'frame_0', poundSpeed - 50);
+  renderFxToImg('AiDust', 200, 'assets/vfx/pound', 11, 'frame_0', poundSpeed - 50);
+
+  setTimeout(() => {
+    audio.currentTime = 0;
+    audio.play();
+  }, poundSpeed - 120);
 
   setTimeout(function () {
     fistArray.forEach((element) => {
       element.style.animation = '';
-      document.body.style.animation = '';
     });
+    document.body.style.animation = '';
     poundCount -= 1;
     if (poundCount > 0) {
-      playAnimation(choice);
+      animateRound();
     }
-  }, 350);
-
-  setTimeout(function () {
     if (poundCount == 1) {
-      console.log(playerChoice);
-      if (playerChoice != 'rock') {
-        hideHand('player', 'fist');
-      }
-      showHand('player', playerChoice);
-
-      let randChoice = parseInt(Math.random() * 3);
-      if (randChoice != 0) {
-        hideHand('ai', 'fist');
-      }
-      showHand('ai', handMap[randChoice]);
-
-      determineWinner(randChoice, handMap.indexOf(playerChoice));
+      setTimeout(showHands, poundSpeed);
     }
-  }, 180);
+  }, poundSpeed + timeBetweenPounds);
+}
+
+function showHands() {
+  if (choice != 'rock') {
+    hideHand('player', 'fist');
+  }
+  showHand('player', choice);
+
+  let randChoice = parseInt(Math.random() * 3);
+  if (randChoice != 0) {
+    hideHand('ai', 'fist');
+  }
+  showHand('ai', handMap[randChoice]);
+
+  determineWinner(randChoice, handMap.indexOf(choice));
+}
+
+function determineWinner(aiNumber, playerNumber) {
+  // play score animation
+  setTimeout(() => {
+    // player's round
+    if (wrapNumber(playerNumber - 1) == aiNumber) {
+      playerScore++;
+      playAnimation('player-images', 'roundWin forwards', attackSpeed);
+    }
+    // ai's round
+    else if (wrapNumber(aiNumber - 1) == playerNumber) {
+      aiScore++;
+      playAnimation('ai-images', 'roundWinAi forwards', attackSpeed);
+    }
+    // draw
+    else {
+      playAnimation('player-images', 'draw forwards', attackSpeed);
+      playAnimation('ai-images', 'drawAi forwards', attackSpeed);
+    }
+  }, attackDelay);
+
+  // reset play field
+  setTimeout(function () {
+    // Check for win
+    if (aiScore > 2 || playerScore > 2) {
+      setTimeout(() => {
+        playAnimation('endScreen', 'endScreen cubic-bezier(0.42, -0.02, 0.82, 0.85) forwards', 100);
+        if (playerScore > 2) {
+          document.getElementById('win').style.visibility = 'visible';
+          winStreak++;
+          document.getElementById('winStreak').innerHTML = 'Win Streak: ' + winStreak;
+        } else {
+          document.getElementById('lose').style.visibility = 'visible';
+          winStreak = 0;
+          document.getElementById('winStreak').innerHTML = 'Win Streak: ' + winStreak;
+        }
+      }, delayEndScreen);
+    } else {
+      buttons.forEach((button) => (button.disabled = false));
+      resetHands();
+    }
+
+    updateScores();
+  }, resetTime + attackDelay + attackSpeed);
 }
 
 function hideHand(parent, _choice) {
@@ -100,48 +161,38 @@ function showHand(parent, _choice) {
   typeArray[0].style.visibility = 'visible';
 }
 
-function determineWinner(aiNumber, playerNumber) {
-  const resetTime = 900;
-  const delayAnimation = 200;
-  setTimeout(() => {
-    if (wrapNumber(playerNumber) == aiNumber) {
-      playerScore++;
+function updateScores() {
+  document.getElementById('ai-score').innerHTML = aiScore;
+  document.getElementById('player-score').innerHTML = playerScore;
+}
 
-      document.getElementById('player-images').style.animation =
-        'roundWin 800ms 100ms ease forwards';
-    } else if (wrapNumber(aiNumber) == playerNumber) {
-      aiScore++;
+function playAnimation(id, animation, duration, reset = true) {
+  let style = document.getElementById(id).style;
+  style.animation = animation;
+  style.animationDuration = duration + 'ms';
 
-      document.getElementById('ai-images').style.animation = 'roundWinAi 800ms 100ms ease forwards';
-    } else {
-      document.getElementById('player-images').style.animation = 'draw 500ms  forwards';
-      document.getElementById('ai-images').style.animation = 'drawAi 500ms   forwards';
-    }
-  }, delayAnimation);
-  setTimeout(function () {
-    document.getElementById('player-images').style.animation = '';
-    document.getElementById('player-score').innerHTML = playerScore;
-    document.getElementById('ai-images').style.animation = '';
-    document.getElementById('ai-score').innerHTML = aiScore;
-  }, resetTime);
+  if (reset) {
+    setTimeout(() => {
+      style.animation = '';
+    }, duration);
+  }
+}
 
-  setTimeout(function () {
-    if (aiScore > 2 || playerScore > 2) {
-      alert('We have a winner!');
-      aiScore = 0;
-      playerScore = 0;
-      setTimeout(function () {
-        document.getElementById('ai-score').innerHTML = aiScore;
-        document.getElementById('player-score').innerHTML = playerScore;
-      }, 0);
-    }
-  }, 1100);
+function resetHands() {
+  hideHand('ai', 'fists');
+  hideHand('ai', 'paper');
+  hideHand('ai', 'scissor');
+  hideHand('player', 'fists');
+  hideHand('player', 'paper');
+  hideHand('player', 'scissor');
+  showHand('player', 'fist');
+  showHand('ai', 'fist');
 }
 
 function wrapNumber(input) {
-  if (input == 0) {
+  if (input == -1) {
     return 2;
   } else {
-    return input - 1;
+    return input;
   }
 }
